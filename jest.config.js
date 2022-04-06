238c4532ba2028c2590135a2f2e8aca6650017cf
goto40 <pierre.bayerl@googlemail.com>
2022-04-06 20:35:27 +0200

    prepared test coverage

--- jest.config.js
+++ jest.config.js
@@ -2,4 +2,14 @@
 module.exports = {
   preset: 'ts-jest',
   testEnvironment: 'node',
+  collectCoverage: true,
+  collectCoverageFrom: [
+    "src/**/*.ts",
+    "!src/extension.ts",
+    "!**/generated/**",
+    "!src/cli/index.ts",
+    "!src/cli/**",
+    "!src/language-server/main.ts",
+  ],
+  moduleFileExtensions: ["ts", "js", "json"],
 };
--- tests/item-language.test.ts
+++ tests/item-language.test.ts
@@ -1,14 +1,13 @@
 import { LangiumDocument } from 'langium';
 import { parseDocument } from 'langium/lib/test';
 import { createItemLanguageServices } from '../src/language-server/item-language-module';
-import { Model, Struct } from '../src/language-server/generated/ast';
+import { Model, ScalarAttribute, Struct } from '../src/language-server/generated/ast';
 import { URI } from 'vscode-uri';
 import { Mode } from 'fs';
 
 const services = createItemLanguageServices().ItemLanguage;
 
 it("test parsing a simple model", () => {
-    console.log("a first test")
     const result = services.shared.workspace.LangiumDocumentFactory.fromString(
         // TODO: make property_set built_in.props implicit
         `
@@ -25,7 +24,8 @@ it("test parsing a simple model", () => {
     expect(model.packages.length).toEqual(1)
     expect(model.packages[0].name).toEqual("first")
     expect(model.packages[0].items.length).toEqual(1)
-    //TODO expect(model.packages[0].items[0]).toBeInstanceOf(Struct)
     const struct = model.packages[0].items[0] as Struct
-    expect(model.packages[0].items[0].name).toEqual("First")
+    expect(struct.name).toEqual("First")
+    expect((struct.attributes[0] as ScalarAttribute).name).toEqual("x")
+    expect((struct.attributes[1] as ScalarAttribute).name).toEqual("y")
 });
